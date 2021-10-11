58920510ec76563c854de02b83760d214abd1e2a
Filipe <filipe.b.braga@gmail.com>
2021-10-11 11:33:00 -0300

    8 an 9 exercise

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/evenOrOdd.js
@@ -0,0 +1,20 @@
+let a = 2;
+let b = 6;
+let c = 8;
+
+
+// is there an even number?
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0){
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+
+//is there an odd number?
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0){
+    console.log(true)
+} else {
+    console.log(false)
+}
+
