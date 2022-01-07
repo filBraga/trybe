667bc6a093dc87ed0abc5f990a4f02d368927c2c
Filipe <filipe.b.braga@gmail.com>
2022-01-07 14:08:03 -0300

    pausing project pokedex

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
-import NextPokemonBtn from './NextPokemonBtn';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
-      <NextPokemonBtn />
     </div>
   );
 }
--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/src/Pokedex.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/src/Pokedex.js
@@ -2,10 +2,28 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state={
+            pokemon: props.pokemons,
+        };
+    }
+
+    createButtonTrype = () => {
+        const { pokemons } = this.props;
+        const pokemonType = new Set(pokemons.map(({type}) => type));
+        const pokeTypesArray = Array.from(pokemonType)
+        return pokeTypesArray.map((type)=>(<button key= { type }>{type}</button>))
+    }
+
     render() {
         return (
             <div className="pokedex">
                 {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <button>All</button>
+                { this.createButtonTrype() }
             </div>
         );
     }
